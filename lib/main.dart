8491758eb2980fa71e32e8ebcdd350992ede642e
Toni Mägel <tmaegel@posteo.de>
2023-07-15 08:59:26 +0200

    Add BlocObserver

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,30 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todotxt/login/login.dart';
 import 'package:todotxt/presentation/router/router.dart';
 
-void main() => runApp(const App());
+void main() {
+  Bloc.observer = SimpleBlocObserver();
+  runApp(const App());
+}
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    debugPrint('${bloc.runtimeType} change');
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    debugPrint('${bloc.runtimeType} transition');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    debugPrint('${bloc.runtimeType} error stackTrace');
+    super.onError(bloc, error, stackTrace);
+  }
+}
 
 class App extends StatelessWidget {
   const App({super.key});
