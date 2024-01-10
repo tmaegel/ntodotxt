a877f396c511be2e3c3760d3741567c2b77c7d4a
Toni Mägel <tmaegel@posteo.de>
2024-01-10 13:50:00 +0100

    style(GenericChoiceChip): Fix spacing issue

--- lib/common_widgets/chip.dart
+++ lib/common_widgets/chip.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
-class GenericChoiceChip extends ChoiceChip {
+class GenericChoiceChip extends StatelessWidget {
+  final Widget label;
+  final bool selected;
+  final bool showCheckmark;
+  final Function(bool selected) onSelected;
+
   const GenericChoiceChip({
-    required super.label,
-    super.selected = false,
-    super.showCheckmark = false,
-    super.onSelected,
+    required this.label,
+    this.selected = false,
+    this.showCheckmark = false,
+    required this.onSelected,
     super.key,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    return ChoiceChip(
+      label: label,
+      selected: selected,
+      showCheckmark: showCheckmark,
+      // Workaround: https://github.com/flutter/flutter/issues/67797
+      visualDensity: const VisualDensity(
+        horizontal: -4.0,
+        vertical: -4.0,
+      ),
+      onSelected: (bool selected) => onSelected(selected),
+    );
+  }
 }
 
 class GenericChipGroup extends StatelessWidget {
@@ -21,9 +41,10 @@ class GenericChipGroup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Wrap(
-      crossAxisAlignment: WrapCrossAlignment.center,
       spacing: 4.0, // gap between adjacent chips
       runSpacing: 4.0, // gap between lines
+      runAlignment: WrapAlignment.center,
+      crossAxisAlignment: WrapCrossAlignment.start,
       children: children,
     );
   }
